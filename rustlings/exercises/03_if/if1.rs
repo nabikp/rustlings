c5c06baf1fd125e18d3ba0192620675a0d1b2eb0
Kalash Pratyay <nabikp2209@gmail.com>
2025-10-21 21:00:30 +0530

    Adding 03_if

--- rustlings/exercises/03_if/if1.rs
+++ rustlings/exercises/03_if/if1.rs
@@ -4,10 +4,17 @@ fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+    if(a >= b){
+        return a;
+    }
+    else  {
+        return b;
+    }
 }
 
 fn main() {
     // You can optionally experiment here.
+
 }
 
 // Don't mind this for now :)
